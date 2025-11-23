4d2f392c289de8767b394e07c34c4269a23be9bf
Viki Lev <vikivikivikikoryagina@gmail.com>
2025-11-23 15:22:38 +0200

    added blog

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BlogRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_default')]
+    public function index(BlogRepository $blogRepository, EntityManagerInterface $manager): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
